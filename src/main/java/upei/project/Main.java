458f75fa811a5fd2fb657a49fcb881e5bb00934c
Zeyad Awad <zeyadahmed@me.com>
2023-11-11 19:54:45 -0400

    test233

--- src/main/java/upei/project/Main.java
+++ src/main/java/upei/project/Main.java
@@ -3,7 +3,9 @@ package upei.project;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-
+    public void print(){
+        System.out.print("Z");
+    }
     private int exampleVar = 0;
 
     public void setExampleVar(int inVal)
