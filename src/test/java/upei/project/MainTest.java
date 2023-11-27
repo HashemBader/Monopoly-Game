b86d15d32f94d0ea2abc7c1c5542d8dbe8772e7c
Zeyad Awad <zeyadahmed@me.com>
2023-11-27 18:48:01 -0400

    ALhamdulillah

--- src/test/java/upei/project/MainTest.java
+++ src/test/java/upei/project/MainTest.java
@@ -13,20 +13,19 @@ public class MainTest {
     @Test
     public void testGame(){
         ArrayList<BoardSquare> map = Driver.createBoard();
-        Player zeyad = new Player(1500);
-        Player hashem = new Player(1500);
+        Player zeyad = new Player(1500, Player.STRATEGY.GREEDY);
+        Player hashem = new Player(1500, Player.STRATEGY.STATION_GUY);
 
-        zeyad.moveN(1);
-        map.get(zeyad.getPos()).playerOnLocation(zeyad);
-        assertEquals(1500-2, zeyad.getMoney());
-        assertEquals(1500+2, hashem.getMoney());
+        hashem.moveN(5);
+        map.get(hashem.getPos()).playerOnLocation(hashem);
+        assertEquals(1440, hashem.getMoney());
 
     }
 
     @Test
     public void testOwnerEquality(){
-        Player p1 = new Player(1500);
-        Player p2 = new Player(1540);
+        Player p1 = new Player(1500,  Player.STRATEGY.GREEDY);
+        Player p2 = new Player(1540, Player.STRATEGY.GREEDY);
         Player p3 = p1;
         System.out.print(p3);
         assertTrue(p1 == p3, "Failed");
@@ -34,9 +33,10 @@ public class MainTest {
     @Test
     public void testOwner(){
         Country egypt = new Country(0, "Egypt", 1000, 190000);
-        Player p1 = new Player(1500);
-        egypt.setOwner(new Player(1000));
+        Player p1 = new Player(1500, Player.STRATEGY.GREEDY);
+        egypt.setOwner(new Player(1000, Player.STRATEGY.GREEDY));
         egypt.playerOnLocation(p1);
+
         assertEquals(egypt.getOwner() == p1, p1.getMoney());
     }
 }
