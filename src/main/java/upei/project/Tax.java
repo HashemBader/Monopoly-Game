f89ab4145d464a8e288da396e1f69fdb08d29048
Hashem <hlbader@upei.ca>
2023-12-03 17:16:31 -0400

    revert sim

--- src/main/java/upei/project/Tax.java
+++ src/main/java/upei/project/Tax.java
@@ -1,24 +1,31 @@
 package upei.project;
 
-public class Tax extends BoardSquare{
-
+/**
+ * The Tax class represents a specific type of BoardSquare where players pay a tax amount.
+ * It extends the BoardSquare abstract class and defines actions taken when a player lands on a Tax square.
+ */
+public class Tax extends BoardSquare {
 
     /**
-     * Constructor that creates a BoardLocation instance of iLoc
-     *
-     * @param iLoc : location on the board
-     * @param name
+     * Constructor to create a Tax instance.
+     * @param iLoc Location index on the board.
+     * @param name Name of the Tax square.
      */
     public Tax(int iLoc, String name) {
         super(iLoc, name);
     }
 
+    /**
+     * Method triggered when a player lands on the Tax square.
+     * Deducts a specific tax amount from the player based on the Tax square's position on the board.
+     * @param player Player object representing the player on the Tax square.
+     */
     @Override
     public void playerOnLocation(Player player) {
         if (player.getPos() == 4){
-            player.subtractMoney(200);
+            player.subtractMoney(200); // Deducts $200 as tax when landing on position 4
         } else if (player.getPos() == 38) {
-            player.subtractMoney(100);
+            player.subtractMoney(100); // Deducts $100 as tax when landing on position 38
         }
     }
 }
